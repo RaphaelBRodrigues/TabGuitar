f2645cb722e4e5794cc832d70b25adf43776cf4c
Raphael Barbosa Rodrigues <42044496+RaphaelBRodrigues@users.noreply.github.com>
2019-06-04 13:12:00 -0300

    Proposta Inicail de tcc

---
+++ js.js
@@ -0,0 +1,28 @@
+	var t=0;
+
+	function Escuro() {
+   
+	  document.getElementById("bodi").style.backgroundColor= "rgba(0,0,0,0.85)";
+   	document.getElementById("cab").style.borderRight="2px solid yellow";
+    document.getElementById("cab").style.backgroundColor="black";
+    document.getElementById("icone-sol").style.color="white";
+    document.getElementById("icone-afinador").style.color="white";
+    document.getElementById("icone-musica").style.color="white";
+    document.getElementById("icone-menu").style.color="white";
+    document.getElementById("icone-pessoa").style.color="white";
+
+
+}
+function Claro(){
+
+
+   	document.getElementById("bodi").style.backgroundColor= "#eef0af";
+    document.getElementById("cab").style.borderRight="2px solid black";
+    document.getElementById("cab").style.backgroundColor="white";
+    document.getElementById("icone-sol").style.color="white";
+    document.getElementById("icone-afinador").style.color="black";
+    document.getElementById("icone-musica").style.color="black";
+    document.getElementById("icone-menu").style.color="black";
+    document.getElementById("icone-pessoa").style.color="black";
+}
+
